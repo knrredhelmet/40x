f373d7b0bdc9b12fb84d87ef3080ecbbccb5c76b
knrredhelmet <redhelmet24@protonmail.com>
2024-12-05 10:58:34 +0530

    latest

--- 40x.sh
+++ 40x.sh
@@ -74,6 +74,7 @@ main() {
         "X-Originating-IP: 127.0.0.1"
         "X-Forwarded: 127.0.0.1"
         "Forwarded-For: 127.0.0.1"
+        "X-Forwarded-For: 127.0.0.1:80"
         "X-Remote-IP: 127.0.0.1"
         "X-Remote-Addr: 127.0.0.1"
         "X-ProxyUser-Ip: 127.0.0.1"
